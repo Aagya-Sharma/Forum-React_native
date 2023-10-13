79b220a1192f6c619956a181f9d4bd03211a4bda
Aagya-Sharma <sharmaaagya7@gmail.com>
2023-10-13 11:42:31 +0545

    forum creation and details page

---
+++ src/screens/ForumCreation.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  Alert,
+} from "react-native";
+import { Card } from "react-native-elements";
+import NextButton from "../components/Button";
+import Titletext from "../components/TitleText";
+import { useForumStore } from "../../store";
+import { shallow } from "zustand/shallow";
+import { useNavigation } from "@react-navigation/native";
+const { width } = Dimensions.get("window");
+
+const ForumCreation = () => {
+  const [newForumTitle, setNewForumTitle] = useState("");
+  const [newForumContent, setNewForumContent] = useState("");
+  const { addForum, user, setUser } = useForumStore();
+  const navigation = useNavigation();
+  const handleAdd = () => {
+    if (newForumTitle && newForumContent && user) {
+      const newForum = {
+        id: Date.now(),
+        title: newForumTitle,
+        content: newForumContent,
+        author: user.username,
+      };
+
+      // Use the addForum action to add the new forum to the Zustand store
+      addForum(newForum);
+
+      // Reset the input fields
+      setNewForumTitle("");
+      setNewForumContent("");
+      Alert.alert("Forum created successfully");
+
+      navigation.navigate("Home");
+    }
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <Titletext text="Create Forum" />
+      <Card containerStyle={styles.cardContainer}>
+        <Text> Forum Title:</Text>
+        <TextInput
+          style={styles.textInput}
+          value={newForumTitle}
+          onChangeText={(text) => setNewForumTitle(text)}
+        />
+        <Text> Forum Content</Text>
+        <TextInput
+          style={styles.textInput}
+          value={newForumContent}
+          onChangeText={(text) => setNewForumContent(text)}
+        />
+      </Card>
+      <NextButton handlePress={handleAdd} text="Create Forum" />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  textInput: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: "#D9D9D9",
+  },
+  cardContainer: {
+    padding: 15,
+    margin: 15,
+  },
+  subcontainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 35,
+  },
+  switchContainer: {
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  nextButtonTitle: {
+    color: "#fff",
+  },
+  nextButton: {
+    width: width * 0.3,
+    borderRadius: 25,
+    margin: 10,
+    padding: 10,
+    alignItems: "center",
+  },
+});
+
+export default ForumCreation;
---
+++ src/screens/ForumDetails.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import { useRoute } from "@react-navigation/native";
+import NavBar from "../components/NavBar";
+import { useForumStore } from "../../store";
+import NextButton from "../components/Button";
+
+function ForumDetails() {
+  const route = useRoute();
+  const { id } = route.params;
+
+  const { forums, comments, addComment } = useForumStore(); // Assuming you have a store set up for state management
+
+  const forum = forums.find((forum) => forum.id === id) || {};
+
+  const [newComment, setNewComment] = useState("");
+  const [commentList, setCommentList] = useState(comments[id] || []);
+
+  const handleComment = () => {
+    if (newComment) {
+      // Use the addComment function from your state management library to add a new comment
+      addComment(id, newComment);
+      setCommentList([...commentList, newComment]);
+      setNewComment("");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <NavBar />
+      <View style={styles.forumDetailsContainer}>
+        <Text style={styles.forumTitle}>{forum.title}</Text>
+        <Text>{forum.content}</Text>
+        <Text style={styles.comments}>Comments</Text>
+        <FlatList
+          data={commentList}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.commentContainer}>
+              <Text style={styles.comment}>{item}</Text>
+            </View>
+          )}
+        />
+        <View style={styles.commentInputContainer}>
+          <TextInput
+            style={styles.commentInput}
+            placeholder="Add a comment"
+            value={newComment}
+            onChangeText={(text) => setNewComment(text)}
+          />
+        </View>
+        <NextButton text="Post Comment" handlePress={handleComment} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  forumDetailsContainer: {
+    padding: 16,
+  },
+  forumTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  comments: {
+    fontSize: 18,
+    marginTop: 12,
+  },
+  comment: {
+    fontSize: 16,
+    marginVertical: 4,
+  },
+  commentInputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
+  },
+  commentInput: {
+    flex: 1,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginRight: 8,
+  },
+  commentContainer: {
+    backgroundColor: "#fff", // Background color for comment boxes
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  comment: {
+    fontSize: 16,
+  },
+});
+
+export default ForumDetails;
