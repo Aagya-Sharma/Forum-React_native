d2257ad35153a10f2d0f9d5a4344060da99233d2
Aagya-Sharma <sharmaaagya7@gmail.com>
2023-10-13 11:42:39 +0545

    define entry route

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/screens/Home";
+import React, { useEffect } from "react";
+import ForumCreation from "./src/screens/ForumCreation";
+import LoginScreen from "./src/screens/LoginScreen";
+import ForumDetails from "./src/screens/ForumDetails";
+import { useForumStore } from "./store";
+const Stack = createStackNavigator();
 
 export default function App() {
+  const { user, loggedIn } = useForumStore();
+  const initialRoute = loggedIn ? "Home" : "LoginScreen";
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={initialRoute}
+        screenOptions={{
+          headerTintColor: "#C1381A",
+          headerTitleStyle: {
+            color: "#ffffff",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="ForumCreation" component={ForumCreation} />
+        <Stack.Screen name="ForumDetails" component={ForumDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
